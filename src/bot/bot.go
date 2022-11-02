6e220299c622dccbe8f582f1ed66891bd2b62886
Giamm9998 <giamm9998@gmail.com>
2022-11-02 01:01:36 +0100

    Now the bot send messages autonomously every n seconds

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -2,48 +2,58 @@ package bot
 
 import (
 	"fmt"
+	"log"
 	"newsBot/config"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/robfig/cron/v3"
 )
 
 var BotID string
-var gobot *discordgo.Session
+var bot *discordgo.Session
+
+const CHANNEL_ID = "1037036075304099843"
+const N_SEC = "3"
 
 func Start() {
-	gobot, error := discordgo.New("Bot " + config.Token)
-	if error != nil {
-		fmt.Println(error.Error())
-		return
-	}
+	bot, BotID = initDiscordBot()
 
-	u, error := gobot.User("@me")
-	if error != nil {
-		fmt.Println(error.Error())
-		return
-	}
-
-	BotID = u.ID
-	gobot.AddHandler(messageHandler)
-
-	error = gobot.Open()
-	if error != nil {
-		fmt.Println(error.Error())
-		return
-	}
+	c := initCronjJob(bot)
+	c.Start()
+	defer c.Stop()
 
 	fmt.Println("Bot is running!")
 
+	//why this???
+	select {}
+
 }
 
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// message has been sent by the bot itself, we don't do anything
-	if m.Author.ID == BotID {
-		return
+func initCronjJob(bot *discordgo.Session) *cron.Cron {
+	c := cron.New()
+
+	//This function must be substuted with the scheduled one that we want to execute
+	sendMessage := func() {
+		bot.ChannelMessageSend(CHANNEL_ID, "Hi!")
+	}
+	c.AddFunc("@every "+N_SEC+"s", sendMessage)
+	return c
+}
+
+func initDiscordBot() (*discordgo.Session, string) {
+	bot, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if m.Content == "Talk" {
-		s.ChannelMessageSend(m.ChannelID, "Hi!")
+	u, err := bot.User("@me")
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	err = bot.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bot, u.ID
 }
--- src/config/config.go
+++ src/config/config.go
@@ -19,13 +19,11 @@ type configStruct struct {
 }
 
 func ReadConfing() error {
-	fmt.Println("Reading the config file")
 	file, error := ioutil.ReadFile("config.json")
 	if error != nil {
 		fmt.Println(error.Error())
 		return error
 	}
-	fmt.Println(string(file))
 
 	error = json.Unmarshal(file, &config)
 	if error != nil {
